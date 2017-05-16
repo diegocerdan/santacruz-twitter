0c005548cebdcc51139ded14e2d1f089278c85a5
Diego Cerdan <diegocerdan@gmail.com>
2017-05-17 00:15:34 +0100

    Adding project files

---
+++ src/twitter-accounts.js
@@ -0,0 +1,124 @@
+
+
+twitter_accounts = {
+    "1" : {
+        "id" : "2389154300",
+        "username" : "santacruz_ayto",
+        "nickname" : "Ayuntamiento"
+    },
+    "2" : {
+        "id" : "2437349802",
+        "username" : "fiestasstacruz",
+        "nickname" : "Fiestas"
+    }, 
+    "3" : {
+        "id" : "347097308",
+        "username" : "policialocalsc",
+        "nickname" : "Policia"
+    },   
+    "4" : {
+        "id" : "445436695",
+        "username" : "cecopalsc",
+        "nickname" : "CECOPAL"
+    },   
+    "5" : {
+        "id" : "2799527441",
+        "username" : "unidadmontessc",
+        "nickname" : "Montes"
+    },  
+    "6" : {
+        "id" : "441765446",
+        "username" : "deportesctfe",
+        "nickname" : "Desportes"
+    },   
+    "7" : {
+        "id" : "476049040",
+        "username" : "laculturaensc",
+        "nickname" : "Cultura"
+    },   
+    "8" : {
+        "id" : "249595722",
+        "username" : "teatroguimera",
+        "nickname" : "Teatro Guimera"
+    },   
+    "9" : {
+        "id" : "2437349802",
+        "username" : "fiestasstacruz",
+        "nickname" : "Fiestas"
+    },  
+    "10" : {
+        "id" : "227247123",
+        "username" : "distritodeanaga",
+        "nickname" : "D. Anaga"
+    },   
+    "11" : {
+        "id" : "365921195",
+        "username" : "centroifara",
+        "nickname" : "D. Centro Ifara"
+    },    
+    "12" : {
+        "id" : "2384291275",
+        "username" : "dissuroeste",
+        "nickname" : "D. SurOeste"
+    },       
+    "13" : {
+        "id" : "2457069311",
+        "username" : "igualdadsc",
+        "nickname" : "Igualdad"
+    },   
+    "14" : {
+        "id" : "702185835567968256",
+        "username" : "scmedioambiente",
+        "nickname" : "Medio Ambiente"
+    },   
+    "15" : {
+        "id" : "2399393230",
+        "username" : "sctributos",
+        "nickname" : "Tributos"
+    },  
+    "16" : {
+        "id" : "163072123",
+        "username" : "scaccesible",
+        "nickname" : "Accesibilidad"
+    },   
+    "17" : {
+        "id" : "278501594",
+        "username" : "cesrsantacruz",
+        "nickname" : "CESR"
+    },   
+    "18" : {
+        "id" : "1021726586",
+        "username" : "santacruzlimpia",
+        "nickname" : "Santa Cruz Limpia"
+    },   
+    "19" : {
+        "id" : "965948040",
+        "username" : "scencarnaval",
+        "nickname" : "Carnaval"
+    },     
+    "20" : {
+        "id" : "155501304",
+        "username" : "socdesarrollo",
+        "nickname" : "Sociedad Desarrollo"
+    },      
+    "21" : {
+        "id" : "2365365410",
+        "username" : "palmetum_tfe",
+        "nickname" : "Palmetum"
+    },   
+    "22" : {
+        "id": "828379286058975232",
+        "username" : "diegocerdanTEST",
+        "nickname" : "TEST ACCOUNT"
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
